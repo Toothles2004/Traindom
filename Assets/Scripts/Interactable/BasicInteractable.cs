05fb14478dacef44265ad030f9b60506c876547f
Leen <leentje2004@hotmail.com>
2023-10-15 10:31:48 +0200

    cooldown for interactions

--- Assets/Scripts/Interactable/BasicInteractable.cs
+++ Assets/Scripts/Interactable/BasicInteractable.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class BasicInteractable : MonoBehaviour
 {
+    [SerializeField]
+    private float _InteractCooldown = 1.0f;
+    private float _InteractTimer = 0.0f;
+    protected bool _DoInteract = false;
+    protected virtual void Update()
+    {
+        if(_InteractTimer < _InteractCooldown)
+        {
+            _InteractTimer += Time.deltaTime;
+        }
+        Debug.Log(_InteractTimer);
+    }
+
     virtual public void Interact()
     {
-        transform.position += Vector3.up * 5;
+        if(_InteractTimer < _InteractCooldown)
+        {
+            _DoInteract = false;
+            return;
+        }
+
+        _DoInteract = true;
+        _InteractTimer = 0.0f;
     }
 }
--- Assets/Scripts/Interactable/SpawnInteractable.cs
+++ Assets/Scripts/Interactable/SpawnInteractable.cs
@@ -9,8 +9,15 @@ public class SpawnInteractable : BasicInteractable
 
     [SerializeField]
     private GameObject _spawnPoint = null;
+
     public override void Interact()
     {
+        base.Interact();
+        if(_DoInteract == false)
+        {
+            return;
+        }
+
         Instantiate(_SpawnObjectTemplate, _spawnPoint.transform.position, _spawnPoint.transform.rotation);
     }
 }
